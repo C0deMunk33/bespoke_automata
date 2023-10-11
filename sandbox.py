c3a4cd5a554cc124199594240178ae102046bb06
C0deMunk33 <hollandcodeandgraphics@gmail.com>
2023-10-11 13:33:48 -0700

    Update sandbox.py

--- sandbox.py
+++ sandbox.py
@@ -1,22 +1,19 @@
 import xml.sax
+import pickle
 
-class WikiHandler(xml.sax.ContentHandler):
-    def __init__(self, target_id):
-        self.target_id = target_id
+class IndexBuilder(xml.sax.ContentHandler):
+    def __init__(self):
         self.current_id = None
         self.in_id_tag = False
-        self.in_text_tag = False
         self.buffer = ""
-        self.article_text = None
+        self.index = {}
 
     def startElement(self, name, attrs):
-        if name == "id" and self.article_text is None:
+        if name == "id":
             self.in_id_tag = True
-        elif name == "text" and self.current_id == self.target_id:
-            self.in_text_tag = True
 
     def characters(self, content):
-        if self.in_id_tag or self.in_text_tag:
+        if self.in_id_tag:
             self.buffer += content
 
     def endElement(self, name):
@@ -24,20 +21,34 @@ class WikiHandler(xml.sax.ContentHandler):
             self.current_id = int(self.buffer.strip())
             self.in_id_tag = False
             self.buffer = ""
-        elif name == "text" and self.in_text_tag:
-            self.article_text = self.buffer
-            self.in_text_tag = False
+        elif name == "page":
+            self.index[self.current_id] = self._locator.getLineNumber()
 
-def get_article(xml_file_path, article_id):
+    def save_index(self, file_path):
+        with open(file_path, 'wb') as file:
+            pickle.dump(self.index, file)
+
+def build_index(xml_file_path, index_file_path):
     parser = xml.sax.make_parser()
-    handler = WikiHandler(article_id)
+    handler = IndexBuilder()
     parser.setContentHandler(handler)
     with open(xml_file_path, 'r', encoding='utf-8') as file:
         parser.parse(file)
-    return handler.article_text
+    handler.save_index(index_file_path)
 
-# Example Usage:
-article_id = 1  # replace with the desired article ID
-xml_file_path = 'your_file.xml'  # replace with the path to your XML file
-article_text = get_article(xml_file_path, article_id)
-print(article_text)
+# Usage:
+# build_index('your_file.xml', 'index.pkl')
+
+def get_article(xml_file_path, index_file_path, article_id):
+    with open(index_file_path, 'rb') as file:
+        index = pickle.load(file)
+    if article_id not in index:
+        return None  # Article ID not found
+    with open(xml_file_path, 'r', encoding='utf-8') as file:
+        file.seek(index[article_id])  # Seek to the start of the article
+        # Now parse the XML from this point to extract the article
+        # ...
+        pass
+
+# Usage:
+# article_text = get_article('your_file.xml', 'index.pkl', 1)
