e7ade3415147b50e5b0f1454b03b9e079cd95b17
C0deMunk33 <hollandcodeandgraphics@gmail.com>
2023-10-11 19:26:02 -0700

    Update sandbox.py

--- sandbox.py
+++ sandbox.py
@@ -1,8 +1,45 @@
 import os
 import re
 import random
+import xml.etree.ElementTree as ET
+
 articles_filename ='enwiki-20231001-pages-articles-multistream.xml'
 articles_index_filename = 'enwiki-20231001-pages-articles-multistream-index.txt'
+
+def index_binary_search(file_path, title_part):
+    # index is formated like this: #section:#id:title and all titles are alphabetically sorted
+    # find the first line that starts with title_part using an alphabetical binary search
+    with open(file_path, 'r', encoding='utf-8') as file:
+        low = 0
+        # file size in bytes
+        max = os.path.getsize(file_path)
+        high = max
+        while low <= high:
+            mid = (low + high) // 2
+            if mid >= max:
+                return None
+            file.seek(mid)
+            try:
+                file.readline()
+                line = file.readline()
+            except:
+                return None
+           
+            line_index = file.tell() - len(line)
+            #split line on :
+            line_parts = line.split(':')
+            id = int(line_parts[1])
+            # title is all parts from index 2 to the end
+            title = ':'.join(line_parts[2:])
+
+            if title_part == title:
+                return id
+            elif title_part < title:
+                high = mid - 1
+            else:
+                low = mid + 1
+
+    return None
 
 def binary_search_xml(file_path, target_id):
     with open(file_path, 'r', encoding='utf-8') as file:
@@ -21,7 +58,6 @@ def binary_search_xml(file_path, target_id):
             except:
                 return None
             
-            print("mid: ", mid)
 
             line = ""
             # read the next line
@@ -49,7 +85,23 @@ def binary_search_xml(file_path, target_id):
             else:
                 high = mid - 1
 
+    return None
+
+id = index_binary_search(articles_index_filename, 'AssistiveTechnology')
+print(id)
+'''
 # do it 10 times
 for i in range(10):
-    random_id = random.randint(0, 300000)
-    print(binary_search_xml(articles_filename, random_id))
+    random_id = random.randint(0, 74955154)
+    article = binary_search_xml(articles_filename, random_id)
+    # parse the article xml into a dictionary
+    if article:
+        root = ET.fromstring(article)
+        article = {}
+        for child in root:
+            article[child.tag] = child.text
+    if(article):
+        print("Article found for id: ", random_id, " with title: ", article['title'])
+    else:
+        print("No article found for id: ", random_id)
+'''
