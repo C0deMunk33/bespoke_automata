afbddc0069b871dd962f9c5a06ab5d8e49a16ac4
C0deMunk33 <hollandcodeandgraphics@gmail.com>
2023-10-17 16:10:13 -0700

    misc

--- sandbox.py
+++ sandbox.py
@@ -1,114 +1,87 @@
-import os
-import re
-import random
-import xml.etree.ElementTree as ET
+#! pip3 install -q towhee pymilvus==2.2.11
+# Download data
+#! wget -q https://github.com/towhee-io/examples/releases/download/data/New_Medium_Data.csv
 
-articles_filename ='enwiki-20231001-pages-articles-multistream.xml'
-articles_index_filename = 'enwiki-20231001-pages-articles-multistream-index.txt'
+import pandas as pd
+from towhee import ops, pipe, DataCollection
+import numpy as np
+import time
+from pymilvus import connections, FieldSchema, CollectionSchema, DataType, Collection, utility
 
-def count_lines(file_path):
-    with open(file_path, 'r', encoding='utf-8') as file:
-        count = sum(1 for _ in file)
-    return count
+df = pd.read_csv('New_Medium_Data.csv', converters={'title_vector': lambda x: eval(x)})
+df.head()
 
-import csv
+connections.connect(host='192.168.0.7', port='19530')
 
-def convert_to_csv(input_file, output_file):
-    with open(input_file, 'r') as infile, open(output_file, 'w', newline='') as outfile:
-        csv_writer = csv.writer(outfile, quoting=csv.QUOTE_MINIMAL)
-        csv_writer.writerow(['Section', 'ID', 'Title'])  # Write header
-        
-        for line in infile:
-            section, id_, title = line.strip().split(':')
-            csv_writer.writerow([section, id_, title])
-            
-def binary_search_title(csv_file, target_title):
-    with open(csv_file, 'r') as file:
-        csv_reader = csv.reader(file)
-        data = list(csv_reader)[1:]  # Exclude header
-        
-        data.sort(key=lambda x: x[2])  # Ensure data is sorted by title
-        
-        left, right = 0, len(data) - 1
-        
-        while left <= right:
-            mid = (left + right) // 2
-            title = data[mid][2]
-            
-            if title == target_title:
-                return data[mid]  # Record found
-            elif title < target_title:
-                left = mid + 1    # Search in the right half
-            else:
-                right = mid - 1   # Search in the left half
-        return None  # Record not found
+def create_milvus_collection(collection_name, dim):
+    if utility.has_collection(collection_name):
+        utility.drop_collection(collection_name)
+    
+    fields = [
+            FieldSchema(name="id", dtype=DataType.INT64, is_primary=True, auto_id=False),
+            FieldSchema(name="title", dtype=DataType.VARCHAR, max_length=500),   
+            FieldSchema(name="title_vector", dtype=DataType.FLOAT_VECTOR, dim=dim),
+            FieldSchema(name="link", dtype=DataType.VARCHAR, max_length=500),
+            FieldSchema(name="reading_time", dtype=DataType.INT64),
+            FieldSchema(name="publication", dtype=DataType.VARCHAR, max_length=500),
+            FieldSchema(name="claps", dtype=DataType.INT64),
+            FieldSchema(name="responses", dtype=DataType.INT64)
+    ]
+    schema = CollectionSchema(fields=fields, description='search text')
+    collection = Collection(name=collection_name, schema=schema)
+    
+    index_params = {
+        'metric_type': "L2",
+        'index_type': "IVF_FLAT",
+        'params': {"nlist": 2048}
+    }
+    collection.create_index(field_name='title_vector', index_params=index_params)
+    return collection
+
+collection = create_milvus_collection('search_article_in_medium', 768)
 
 
 
 
 
+insert_pipe = (pipe.input('df')
+                   .flat_map('df', 'data', lambda df: df.values.tolist())
+                   .map('data', 'res', ops.ann_insert.milvus_client(host='192.168.0.7', 
+                                                                    port='19530',
+                                                                    collection_name='search_article_in_medium'))
+                   .output('res')
+)
 
-def binary_search_xml(file_path, target_id):
-    with open(file_path, 'r', encoding='utf-8') as file:
-        low = 0
-        # file size in bytes
-        max = os.path.getsize(file_path)
-        high = max
-        while low <= high:
-            mid = (low + high) // 2
-            if mid >= max:
-                return None
-            file.seek(mid)
-            try:
-                file.readline()
-                line = file.readline()
-            except:
-                return None
-            
 
-            line = ""
-            # read the next line
-            while '<page>' not in line:
-                #if line contains end of file character, break
-                if file.tell() >= max:
-                    return None           
-                line = file.readline()
-            page_start_idx = file.tell() - len(line)
+start_time = time.time()
 
-            # move three lines down
-            for i in range(3):
-                line = file.readline()
-            # get the id from the line
-            id = int(re.search(r'<id>(\d+)</id>', line).group(1))
-            if id == target_id:
-                # seek to the start of the page, get all text between <page and </page>
-                file.seek(page_start_idx)
-                page = ""
-                while '</page>' not in page:
-                    page += file.readline()
-                return page                   
-            elif id < target_id:
-                low = mid + 1
-            else:
-                high = mid - 1
+_ = insert_pipe(df)
 
-    return None
+end_time = time.time()
+print('Time Cost: {}'.format(end_time - start_time))
 
-id = index_binary_search(articles_index_filename, 'AssistiveTechnology')
-print(id)
-'''
-# do it 10 times
-for i in range(10):
-    random_id = random.randint(0, 74955154)
-    article = binary_search_xml(articles_filename, random_id)
-    # parse the article xml into a dictionary
-    if article:
-        root = ET.fromstring(article)
-        article = {}
-        for child in root:
-            article[child.tag] = child.text
-    if(article):
-        print("Article found for id: ", random_id, " with title: ", article['title'])
-    else:
-        print("No article found for id: ", random_id)
-'''
+collection.load()
+collection.num_entities
+
+
+
+
+
+search_pipe = (pipe.input('query')
+                    .map('query', 'vec', ops.text_embedding.dpr(model_name="facebook/dpr-ctx_encoder-single-nq-base"))
+                    .map('vec', 'vec', lambda x: x / np.linalg.norm(x, axis=0))
+                    .flat_map('vec', ('id', 'score'), ops.ann_search.milvus_client(host='192.168.0.7', 
+                                                                                   port='19530',
+                                                                                   collection_name='search_article_in_medium'))  
+                    .output('query', 'id', 'score')
+               )
+
+res = search_pipe('monkey')
+DataCollection(res).show()
+
+
+
+res = search_pipe.batch(['funny python demo', 'AI in data analysis'])
+for re in res:
+    DataCollection(re).show()
+
