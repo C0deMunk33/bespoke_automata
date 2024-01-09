2cf8bbaac1c602addc0fcb8e83291cd5aad216c3
C0deMunk33 <hollandcodeandgraphics@gmail.com>
2024-01-08 18:59:46 -0700

    whisper

--- APIs/whisper_api.py
+++ APIs/whisper_api.py
@@ -21,9 +21,7 @@ class WhisperAPI:
 
 whisper_api = WhisperAPI()
 
-def save_audio_as_mp3(audio_file, filename):
-    audio_bytes = BytesIO()
-    audio_file.save(audio_bytes)
+def save_audio_as_mp3(audio_bytes, filename):
     audio_bytes.seek(0)
     sound = AudioSegment.from_file(audio_bytes)
     mp3_filename = f"./saved_audio_files/{filename}.mp3"
@@ -51,15 +49,15 @@ def list_files():
 def whisper_route():
     # Retrieve the file from the request
     audio_file = request.files["audio"]
-
-    # Save the audio file as an MP3
-    filename = audio_file.filename.rsplit('.', 1)[0]
-    save_audio_as_mp3(audio_file, filename)
-
-    # Convert the file to a BytesIO object for transcription
     audio_bytes = BytesIO()
     audio_file.save(audio_bytes)
     audio_bytes.seek(0)
+    # Save the audio file as an MP3
+    filename = audio_file.filename.rsplit('.', 1)[0]
+    save_audio_as_mp3(audio_bytes, filename)
+
+    # Convert the file to a BytesIO object for transcription
+    
 
     # Load the audio file as a NumPy array
     data, samplerate = sf.read(audio_bytes)
