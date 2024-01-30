68433f4fe90f4b659123530a6bb9fda618f625ff
C0deMunk33 <hollandcodeandgraphics@gmail.com>
2024-01-30 16:24:20 -0700

    streamdiffusion

---
+++ APIs/streamdiffusion_api.py
@@ -0,0 +1,46 @@
+import torch
+from diffusers import AutoencoderTiny, StableDiffusionPipeline
+
+from streamdiffusion import StreamDiffusion
+from streamdiffusion.image_utils import postprocess_image
+
+# You can load any models using diffuser's StableDiffusionPipeline
+pipe = StableDiffusionPipeline.from_pretrained("KBlueLeaf/kohaku-v2.1").to(
+    device=torch.device("cuda"),
+    dtype=torch.float16,
+)
+
+# Wrap the pipeline in StreamDiffusion
+# Requires more long steps (len(t_index_list)) in text2image
+# You recommend to use cfg_type="none" when text2image
+stream = StreamDiffusion(
+    pipe,
+    t_index_list=[0, 16, 32, 45],
+    torch_dtype=torch.float16,
+    cfg_type="none",
+)
+
+# If the loaded model is not LCM, merge LCM
+stream.load_lcm_lora()
+stream.fuse_lora()
+# Use Tiny VAE for further acceleration
+stream.vae = AutoencoderTiny.from_pretrained("madebyollin/taesd").to(device=pipe.device, dtype=pipe.dtype)
+# Enable acceleration
+pipe.enable_xformers_memory_efficient_attention()
+
+
+prompt = "1girl with dog hair, thick frame glasses"
+# Prepare the stream
+stream.prepare(prompt)
+
+# Warmup >= len(t_index_list) x frame_buffer_size
+for _ in range(4):
+    stream()
+
+# Run the stream infinitely
+while True:
+    x_output = stream.txt2img()
+    postprocess_image(x_output, output_type="pil")[0].show()
+    input_response = input("Press Enter to continue or type 'stop' to exit: ")
+    if input_response == "stop":
+        break
